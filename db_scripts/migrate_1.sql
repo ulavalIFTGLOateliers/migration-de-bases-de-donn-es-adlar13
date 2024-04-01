8aeef3637c840b884f6309a43afbfe2d54127f90
Adnane Lardi <adnanelardi9@gmail.com>
2024-03-31 21:00:41 -0400

    commit final

---
+++ db_scripts/migrate_1.sql
@@ -0,0 +1,42 @@
+show tables;
+CREATE TABLE IF NOT EXISTS band (
+    bandName VARCHAR(50) PRIMARY KEY,
+    creation YEAR,
+    genre VARCHAR(50)
+);
+INSERT INTO band (bandname, creation, genre)
+VALUES ('Crazy duo', 2015, 'rock'), ('Luna', 2009, 'classical'), ('Mysterio', 2019, 'pop');
+
+
+Alter table singer Rename to musician;
+ALTER TABLE musician ADD COLUMN role VARCHAR(50);
+ALTER TABLE musician CHANGE COLUMN singerName musicianName VARCHAR(50);
+ALTER TABLE musician ADD COLUMN bandName VARCHAR(50);
+
+UPDATE musician
+SET role = 'vocals',
+    bandName = 'Crazy Duo'
+WHERE musicianName = 'Alina';
+
+
+UPDATE musician
+SET role = 'guitar',
+    bandName = 'Mysterio'
+WHERE musicianName = 'Mysterio';
+
+UPDATE musician
+SET role = 'percussion',
+    bandName = 'Crazy Duo'
+WHERE musicianName = 'Rainbow';
+
+
+UPDATE musician
+SET role = 'piano',
+    bandName = 'Luna'
+WHERE musicianName = 'Luna';
+
+UPDATE musician
+SET role = 'vocals',
+    bandName = 'Crazy Duo'
+WHERE musicianName = 'Alina';
+show tables;
